0ec0e82fe097f5d843c78d805917d6312f4392da
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2022-02-03 17:22:31 +0300

    init first 7 lessons

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,25 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import {Recipe} from "../recipe.model";
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  @Output() recipeWasSelected = new EventEmitter<Recipe>();
+   recipes: Recipe[] = [
+    new Recipe("A test recipe", "this is simply test","https://images.wsj.net/im-121422"),
+    new Recipe("A test recipe", "this is simply test","https://images.wsj.net/im-121422")
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onRecipeSelected(recipe: Recipe){
+    this.recipeWasSelected.emit(recipe)
+  }
+
+}
