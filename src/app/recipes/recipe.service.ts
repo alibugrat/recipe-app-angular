fbf1c27b58f92960bc113002369661ec383ea8f1
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2022-02-08 13:53:39 +0300

    add recipe's form functionality

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -2,9 +2,11 @@ import {Injectable} from "@angular/core";
 import {Recipe} from "./recipe.model";
 import {Ingredient} from "../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list/shopping-list.service";
+import {Subject} from "rxjs";
 
 @Injectable()
 export class RecipeService {
+  recipesChanged = new Subject<Recipe[]>();
 
   private recipes: Recipe[] = [
     new Recipe("Tasty Schnitzel", "A super testy Schnitzel - just awesome", "http://www.buseterim.com.tr/upload/default/2021/1/15/schnitzel680.jpg",
@@ -28,4 +30,18 @@ export class RecipeService {
   this.slService.addIngredients(ingredients);
   }
 
+  addRecipe(recipe: Recipe){
+    this.recipes.push(recipe)
+    this.recipesChanged.next(this.recipes.slice())
+  }
+
+  updateRecipe(index: number, recipe: Recipe){
+    this.recipes[index] = recipe
+    this.recipesChanged.next(this.recipes.slice())
+  }
+
+  deleteRecipe(index: number){
+    this.recipes.splice(index, 1);
+    this.recipesChanged.next(this.recipes.slice())
+  }
 }
