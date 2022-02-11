c55cf339c2b4c70b0b18684467134f93b5923937
Ali Buğra Topçuoğlu <alibugrat@gmail.com>
2022-02-11 15:48:25 +0300

    add authentication

---
+++ src/app/auth/auth.component.ts
@@ -0,0 +1,78 @@
+import {Component, OnDestroy, ViewChild} from "@angular/core";
+import {NgForm} from "@angular/forms";
+import {AuthResponseData, AuthService} from "./auth.service";
+import {Observable, Subscription} from "rxjs";
+import {Router} from "@angular/router";
+import {AlertComponent} from "../shared/alert/alert.component";
+import {PlaceholderDirective} from "../shared/placeholder/placeholder.directive";
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: 'auth.component.html'
+})
+export class AuthComponent implements OnDestroy{
+  isLoginMode = true;
+  isLoading = false;
+  error: string = null;
+  @ViewChild(PlaceholderDirective) alertHost: PlaceholderDirective;
+
+  private closeSub: Subscription;
+
+  constructor(private autService: AuthService, private router: Router) {
+  }
+
+  onSwitchMode() {
+    this.isLoginMode = !this.isLoginMode;
+  }
+
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+
+    const email = form.value.email;
+    const password = form.value.password
+
+    let autObs: Observable<AuthResponseData>;
+
+    this.isLoading = true;
+    if (this.isLoginMode) {
+      autObs = this.autService.login(email, password)
+    } else {
+      autObs = this.autService.singUp(email, password)
+    }
+
+    autObs.subscribe(resData => {
+      console.log(resData)
+      this.isLoading = false;
+      this.router.navigate(['/recipes'])
+    }, error => {
+      console.log(error)
+      this.error = error;
+      this.showErrorAlert(error);
+      this.isLoading = false;
+    })
+
+    form.reset();
+  }
+
+  private showErrorAlert(error: string) {
+    const hostViewContainerRef = this.alertHost.viewContainerRed;
+    hostViewContainerRef.clear();
+
+    const componentRef = hostViewContainerRef.createComponent(AlertComponent);
+
+    componentRef.instance.message = error;
+    this.closeSub = componentRef.instance.close.subscribe(() => {
+      this.closeSub.unsubscribe();
+      hostViewContainerRef.clear();
+
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.closeSub){
+      this.closeSub.unsubscribe();
+    }
+  }
+}
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -8,12 +8,7 @@ import {Subject} from "rxjs";
 export class RecipeService {
   recipesChanged = new Subject<Recipe[]>();
 
-  private recipes: Recipe[] = [
-    new Recipe("Tasty Schnitzel", "A super testy Schnitzel - just awesome", "http://www.buseterim.com.tr/upload/default/2021/1/15/schnitzel680.jpg",
-      [new Ingredient("Meat", 1), new Ingredient("French Fries", 20)]),
-    new Recipe("Big Fat Burger", "What else you need to say?", "https://media-cdn.tripadvisor.com/media/photo-s/1b/9e/a5/ea/zapata-burger.jpg",
-      [new Ingredient("Buns", 2), new Ingredient("Meat", 1)])
-  ];
+  private recipes: Recipe[] = [];
 
   constructor(private slService: ShoppingListService) {
   }
@@ -22,25 +17,30 @@ export class RecipeService {
     return this.recipes.slice();
   }
 
-  getRecipe(id: number){
+  getRecipe(id: number) {
     return this.recipes.slice()[id]
   }
 
   addIngredientsToShoppingList(ingredients: Ingredient[]) {
-  this.slService.addIngredients(ingredients);
+    this.slService.addIngredients(ingredients);
   }
 
-  addRecipe(recipe: Recipe){
-    this.recipes.push(recipe)
-    this.recipesChanged.next(this.recipes.slice())
+  addRecipe(recipe: Recipe) {
+    this.recipes.push(recipe);
+    this.recipesChanged.next(this.recipes.slice());
   }
 
-  updateRecipe(index: number, recipe: Recipe){
+  setRecipes(recipes: Recipe[]) {
+    this.recipes = recipes;
+    this.recipesChanged.next(this.recipes.slice());
+  }
+
+  updateRecipe(index: number, recipe: Recipe) {
     this.recipes[index] = recipe
     this.recipesChanged.next(this.recipes.slice())
   }
 
-  deleteRecipe(index: number){
+  deleteRecipe(index: number) {
     this.recipes.splice(index, 1);
     this.recipesChanged.next(this.recipes.slice())
   }
